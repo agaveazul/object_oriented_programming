ee4fc8f53eac615156a3e548a8babc55c092c0b8
Richard Strauss <rstrauss91@gmail.com>
2017-01-18 13:44:08 -0500

    Completed E1

---
+++ exercise1.rb
@@ -0,0 +1,18 @@
+class BankAccount
+
+attr_accessor :balance, :interest_rate
+
+  def initialize(balance, interest_rate)
+  @balance = balance
+  @interest_rate = interest_rate
+  end
+
+  def deposit(deposit_amt)
+    @balance = @balance + deposit_amt
+  end
+
+  def withdraw(withdraw_amt)
+    @balance = @balance - withdraw_amt
+  end
+
+end
